e3b5757aa967755e60a4dd077b1fc7eb7ca6d17e
Not Archiey <notarchiey@protonmail.com>
2020-04-20 14:17:23 +0700

    Add dialog command (#15)
    * Added dialog Command
    
    * Added Fonts
    
    * Added character images for Dialog command
    
    * added keitaro to characters
    
    * corrected keitaro
    
    I'm clumsy...
    
    * Added jimp to dependency
    
    * Fixed font sizing
    
    * Fixed regex for mentions, emoji, and emotes
    
    * Added character limit check
    
    * made the character and BG images separate files
    
    * fixing name typo
    
    * Fixed extra comma on package.json
    
    * Rewritten for OOP
    
    * Added background argument
    
    * Removed smallest font and measure latency
    
    * Using a JSON file for bg and chars
    
    * Create cave.png
    
    * More user-knowable error message
    
    * Added cave to backgrounds.json
    
    * Fixed error message
    
    * Line overlapping fix
    
    * Created GenerateAll command
    
    * Pre-render images on startup
    
    * Massive Fixes
    
    * Moved jsons one folder up
    
    * Args less than 2 message fix
    
    * More Fixes
    
    * Small cleanup
    
    * Working and Mergeable
    
    * Fixes i guess
    
    * Efficient Message
    
    * Add api for new image generation
    
    * jimp bad imagick good
    
    * removed jimp from package.json
    
    * Added Yuuto for dialog maker
    
    * Revert "Merge branch 'feature/dialog-maker' of https://github.com/Yuuto-Project/yuuto-bot into feature/dialog-maker"
    
    This reverts commit c22b62bb9d4594f596b48a6fba4ec2afe80ff91d, reversing
    changes made to 6f2affc8a1ac443c8d164c84fa51fc4ad3168f43.
    
    * Revert "Revert "Merge branch 'feature/dialog-maker' of https://github.com/Yuuto-Project/yuuto-bot into feature/dialog-maker""
    
    This reverts commit 649781cbf74b72180864e0a4ea75d1b91d85c2a4.
    
    * added Yuuto to vharavters.json
    
    * corrected characters.json
    
    * Removed images
    
    * check if too many requests
    
    * try catch safety
    
    * error handling
    
    * some fixes
    
    * Added knox
    
    * simplify code
    
    * Line cleaning
    
    * Spring cleaning
    
    Co-authored-by: Not Archiey <notarchiey@protonmail.com>
    Co-authored-by: zsotroav <zsotroav@gmail.com>
    Co-authored-by: duncte123 <duncan.sterken123@gmail.com>
    Co-authored-by: Leguu <33807061+Leguu@users.noreply.github.com>

---
+++ commands/Dialog.js
@@ -0,0 +1,166 @@
+import { MessageAttachment } from "@yuuto-project/discord.js";
+import axios from "axios";
+import {
+  EMOJI_REGEX,
+  EMOTE_MENTIONS_REGEX,
+  NONASCII_REGEX
+} from "../utils/discordPatterns.js";
+import Command from "./base/Command.js";
+
+import { sendTyping } from "../utils/utils.js";
+
+const backgrounds = [
+  "bath",
+  "beach",
+  "cabin",
+  "camp",
+  "cave",
+  "forest",
+  "messhall"
+];
+
+const characters = [
+  "aiden",
+  "avan",
+  "chiaki",
+  "connor",
+  "eduard",
+  "felix",
+  "goro",
+  "hiro",
+  "hunter",
+  "jirou",
+  "keitaro",
+  "kieran",
+  "knox",
+  "lee",
+  "naoto",
+  "natsumi",
+  "seto",
+  "taiga",
+  "yoichi",
+  "yoshi",
+  "yuri",
+  "yuuto"
+];
+
+export default class Dialog extends Command {
+  constructor() {
+    const backgroundString = backgrounds.join("`, `");
+    const charactersString = characters.join("`, `");
+
+    super({
+      name: "dialog",
+      category: "fun",
+      description:
+        "Returns an image of a character in Camp Buddy saying anything you want",
+      usage: `Run \`dialog <background> <character> <message>\` or \`dialog <character> <message>\` to give you an image of a Camp Buddy character with a custom dialog!\nAvailable backgrounds : \`${backgroundString}\`\nAvailable characters : \`${charactersString}\``
+    });
+  }
+
+  async run(_client, message, args) {
+    const { channel } = message;
+
+    await sendTyping(channel);
+
+    const now = Date.now();
+
+    if (args.length < 2) {
+      channel.send(
+        "Sorry, but you need to provide the character and the message."
+      );
+      return;
+    }
+
+    let character = args.shift().toLowerCase();
+    let background;
+
+    if (characters.includes(character)) {
+      background = "camp";
+    } else {
+      background = character;
+      character = args.shift().toLowerCase();
+    }
+
+    if (!backgrounds.includes(background)) {
+      channel.send(
+        `Sorry, but I couldn't find \`${background}\` as a location`
+      );
+      return;
+    }
+
+    if (!characters.includes(character)) {
+      channel.send(
+        `Sorry, but I don't think that \`${character}\` is a character in Camp Buddy`
+      );
+      return;
+    }
+
+    if (args.length < 0) {
+      channel.send("Please provide a message to send.");
+      return;
+    }
+
+    // Gets the message by getting the rest of the args
+    const text = args.join(" ");
+
+    // Check if message is more than 120 chars
+    if (text.length > 120) {
+      channel.send(
+        "Sorry, the message limit is 120 characters <:hiroJey:692008426842226708>"
+      );
+      return;
+    }
+
+    // Tests if message includes emoji or emotes
+    if (
+      EMOJI_REGEX.test(text) ||
+      EMOTE_MENTIONS_REGEX.test(text) ||
+      NONASCII_REGEX.test(text)
+    ) {
+      channel.send(
+        "Sorry, I can't display emotes, mentions, or non-latin characters"
+      );
+      return;
+    }
+
+    try {
+      const response = await axios.post(
+        "https://yuuto.dunctebot.com/dialog",
+        { background, character, text },
+        {
+          responseType: "arraybuffer",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json"
+          }
+        }
+      );
+
+      const attachment = new MessageAttachment(
+        Buffer.from(response.data),
+        "result.png"
+      );
+
+      console.debug(
+        `Generated image for ${character} at ${background}, took ${Date.now() -
+          now}ms`
+      );
+
+      message.reply("here you go~!", attachment);
+    } catch (err) {
+      if (err.response.status === 429) {
+        channel.send(
+          "I can't handle this much load at the moment, maybe try again later? <:YoichiPlease:692008252690530334>"
+        );
+        return;
+      }
+
+      channel.send(
+        `An error just happened in me, blame the devs <:YoichiLol:701312070880329800> - ${
+          JSON.parse(err.response.data).message
+        }`
+      );
+    }
+  }
+}
